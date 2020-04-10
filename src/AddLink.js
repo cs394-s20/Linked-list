c73fa7b38b8f27ac0488a42875216c30b917007b
Karen Bao <karenbao2021@u.northwestern.edu>
2020-04-09 22:46:21 -0700

    added some styling changes

--- src/AddLink.js
+++ src/AddLink.js
@@ -4,43 +4,49 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import 'rbx/index.css';
 import { Button, Modal, Field, Label, Control, Input, Container } from 'rbx';
-
+import './index.css';
 
 const OpenModal = () => {
   // const link = prompt('Enter link here')
   return (
-    <div className="ModalWrapper">
-      <Modal.Background />
-        <Modal.Content>
-        <Field>
-          <Label>Link Title</Label>
-          <Control>
-            <Input type="text" placeholder="Google" />
-          </Control>
-        </Field>
-        <Field>
-          <Label>Link URL</Label>
-          <Control>
-            <Input type="text" placeholder="www.google.com" />
-          </Control>
-        </Field>
-        </Modal.Content>
-        <Modal.Close onClick = { () => document.getElementById("openModal").style.display="none"}/>
-      
+    <div style={{color: "white !important"}}>
+        <Modal.Background />
+          <Modal.Content>
+          <Field>
+            <Label style={{color:"white !important"}}>Link Title</Label>
+            <Control>
+              <Input type="text" placeholder="Google" />
+            </Control>
+          </Field>
+          <Field>
+            <Label>Link URL</Label>
+            <Control>
+              <Input type="text" placeholder="www.google.com" />
+            </Control>
+          </Field>
+          </Modal.Content>
+          <Button.Group style={{paddingTop:"10px"}} align="centered">
+            <Button onClick = { () => document.getElementById("openModal").style.display="none"}>Cancel</Button>
+            <Button>Add Link</Button>
+          </Button.Group>
     </div>
 
   )
 };
 
-
 const AddLink = ({}) => {
   
+  const openForm = () => {
+    document.getElementById("openModal").style.display="block";
+    document.getElementById("add-link").style.display="None";
+  }
+
   return (
     <Container>
       <div id="openModal" style={{ display: "None" }}>
         <OpenModal></OpenModal>
       </div>
-      <Button onClick = { () => document.getElementById("openModal").style.display="block" } >
+      <Button id="add-link" onClick = { () =>  openForm()} >
         Add Link
       </Button>
     </Container>
