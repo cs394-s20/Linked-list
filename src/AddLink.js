87a56c6163c100c88f4619bf4fbfc1e5ff8dea17
Kiara McNulty <kiaramcnulty@gmail.com>
2020-04-09 22:24:35 -0700

    Modal popup works sort of

--- src/AddLink.js
+++ src/AddLink.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/database';
-import { Button } from 'rbx';
+import 'rbx/index.css';
+import { Button, Modal, Field, Label, Control, Input, Container } from 'rbx';
 
 
+const OpenModal = () => {
+  // const link = prompt('Enter link here')
+  return (
+    <div className="ModalWrapper">
+      <Modal.Background />
+        <Modal.Content>
+        <Field>
+          <Label>Link Title</Label>
+          <Control>
+            <Input type="text" placeholder="Google" />
+          </Control>
+        </Field>
+        <Field>
+          <Label>Link URL</Label>
+          <Control>
+            <Input type="text" placeholder="www.google.com" />
+          </Control>
+        </Field>
+        </Modal.Content>
+        <Modal.Close onClick = { () => document.getElementById("openModal").style.display="none"}/>
+      
+    </div>
 
-const AddLink = ({}) => (
+  )
+};
 
-    <Button>Click me!</Button>
+
+const AddLink = ({}) => {
   
-  );
+  return (
+    <Container>
+      <div id="openModal" style={{ display: "None" }}>
+        <OpenModal></OpenModal>
+      </div>
+      <Button onClick = { () => document.getElementById("openModal").style.display="block" } >
+        Add Link
+      </Button>
+    </Container>
+  )
+
+};
 
 export default AddLink;
