273e65382b57c91ff8ce9c585593df67afd26e0e
Kiara McNulty <kiaramcnulty@gmail.com>
2020-05-05 18:42:42 -0700

    update link modal to material UI

--- src/Components/AddLink.js
+++ src/Components/AddLink.js
@@ -3,98 +3,59 @@ import '../App.css';
 import firebase from 'firebase/app';
 import 'firebase/database';
 import 'rbx/index.css';
-import { Button, Modal, Field, Label, Control, Input, Container } from 'rbx';
 import '../index.css';
 import AddIcon from '@material-ui/icons/Add';
 import './colors.css';
+import { Modal, Container, Button, InputLabel, TextField, ButtonGroup} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+var folderColor = "#DCDFE7"
 
-const closeModal = () => {
-  document.getElementById("openModal").style.display="none";
-  document.getElementById("add-link").style.display="block";
-  document.getElementById("add-folder").style.display="block";
-}
-
-const updateJSON = ( { state, userState }, color ) => {
-  
-  //might break 
-  //var newItemKey = firebase.database().ref().child('items').push().key;
-
-  //console.log(state.path);
-  // Get a key for a new Post.
-  
-  const userUID = userState.user.uid;
-  var newItemKey = firebase.database().ref("users").child(userUID).push().key;
-  
-  var item = {
-    "name": document.getElementById('linkTitle').value,
-    "type": "link",
-    "path": state.path,
-    "url": document.getElementById('linkUrl').value,
-    "note": document.getElementById("linkNote").value,
-    "id": newItemKey,
-    "color": color
-  };
-
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  firebase.database().ref("users/" + userUID + "/" + newItemKey).set(item);
-  closeModal();
-  document.getElementById('linkTitle').value = "";
-  document.getElementById('linkUrl').value = "";
-  return;
-}
-
-const OpenModal = ( { state, userState }) => {
-//console.log(state.path);
-  var folderColor = "#DCDFE7"
-
-  const handleColor = (color) => {
-    folderColor = color;
-
-  };
-  return (
-    <div style={{color: "white !important"}}>
-        <Modal.Background />
-          <Modal.Content>
-          <Field>
-            <Label style={{color:"white !important"}}>Link Title</Label>
-            <Control>
-              <Input id="linkTitle" type="text" placeholder="Google" />
-            </Control>
-          </Field>
-          <Field>
-            <Label>Link URL</Label>
-            <Control>
-              <Input id="linkUrl" type="text" placeholder="must be full url, ex: https://www.google.com"                />
-            </Control>
-          </Field>
-          <Field>
-            <Label>Link Note</Label>
-            <Control>
-              <Input id="linkNote" type="text" placeholder="optional note (50 char limit)" maxlength="50"/>
-            </Control>
-          </Field>
-          <Label style={{color:"white !important"}}>Link Color</Label>
-          <Button.Group style={{paddingTop:"10px"}} >
-          <Button id="red-btn" onClick ={() => handleColor("#e64343")}/>
-              <Button id="yellow-btn" onClick ={() => handleColor("#f2e874")}/>
-              <Button id="green-btn" onClick ={() => handleColor("#24960e")}/>
-              <Button id="aqua-btn" onClick ={() => handleColor("#43e6b5")}/>
-              <Button id="blue-btn" onClick ={() => handleColor("#3C72DE")}/>
-              <Button id="purple-btn" onClick ={() => handleColor("#7b1da3")}/>
-              <Button id="pink-btn" onClick ={() => handleColor("#D23CDE")}/>
-          </Button.Group>
-          </Modal.Content>
-          <Button.Group style={{paddingTop:"10px"}} align="centered">
-            <Button onClick = { () => closeModal()}>Cancel</Button>
-            <Button color="link" onClick = { () => updateJSON({state, userState}, folderColor)}>Add Link</Button>
-          </Button.Group>
-    </div>
-
-  )
+const handleColor = (color) => {
+  folderColor = color;
 };
 
 const AddLink = ( { state, userState } ) => {
+
+  function rand() {
+    return Math.round(Math.random() * 20) - 10;
+  }
+  
+  function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   //console.log(state);
   const openForm = () => {
     document.getElementById("openModal").style.display="block";
@@ -102,15 +63,64 @@ const AddLink = ( { state, userState } ) => {
     document.getElementById("add-folder").style.display="None";
   }
 
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Add Link</h2>
+      <TextField id="linkTitle" label="Link Title" />
+      <TextField id="linkUrl" label="Link URL" helperText="must be full url, ex: https://www.google.com"/>
+      <TextField id="linkNote" label="Note" helperText="optional note (50 char limit)" inputProps={{ maxLength: 50, }}/>
+      <h1>Link Color</h1>
+      <ButtonGroup style={{paddingTop:"10px"}} >
+          <Button id="red-btn" onClick ={() => handleColor("#e64343")}/>
+          <Button id="yellow-btn" onClick ={() => handleColor("#f2e874")}/>
+          <Button id="green-btn" onClick ={() => handleColor("#24960e")}/>
+          <Button id="aqua-btn" onClick ={() => handleColor("#43e6b5")}/>
+          <Button id="blue-btn" onClick ={() => handleColor("#3C72DE")}/>
+          <Button id="purple-btn" onClick ={() => handleColor("#7b1da3")}/>
+          <Button id="pink-btn" onClick ={() => handleColor("#D23CDE")}/>
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button onClick = {handleClose}>Cancel</Button>
+        <Button onClick = { () => updateJSON({state, userState}, folderColor)}>Add Link</Button>
+      </ButtonGroup>
+    </div>
+  );
+
+  const updateJSON = ( { state, userState }, color ) => {
+    const userUID = userState.user.uid;
+    var newItemKey = firebase.database().ref("users").child(userUID).push().key;
+    
+    var item = {
+      "name": document.getElementById('linkTitle').value,
+      "type": "link",
+      "path": state.path,
+      "url": document.getElementById('linkUrl').value,
+      "note": document.getElementById("linkNote").value,
+      "id": newItemKey,
+      "color": color
+    };
+  
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    firebase.database().ref("users/" + userUID + "/" + newItemKey).set(item);
+    handleClose();
+    document.getElementById('linkTitle').value = "";
+    document.getElementById('linkUrl').value = "";
+    return;
+  }
+
   return (
     <Container >
-      <div id="openModal" style={{ display: "None" }}>
-        <OpenModal state={ state } userState= { userState } ></OpenModal>
-      </div>
-      <Button id="add-link" onClick = { () =>  openForm()} >
-        <AddIcon />
-        Link
+
+      <Button variant="contained" type="button" onClick={handleOpen}>
+        Add Link
       </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+      >
+        {body}
+      </Modal>
     </Container>
   )
 
